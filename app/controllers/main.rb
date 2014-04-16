af941ca5f4c8eb66e684a868ef498d1559e1d0f0
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-04-16 11:18:14 -0700

    Facebook share

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -6,6 +6,9 @@ Ulysess::App.controllers  do
     @vimeo_video_id = @video.link[/\d+/]
     @comments = @video.comments
     @comment = Comment.new
+    @fb_url = "http://ulysses.it"
+    @fb_title = "Interview with #{@title}"
+    @fb_video = @video.link
     render "layouts/index.erb"
   end
   
@@ -20,6 +23,9 @@ Ulysess::App.controllers  do
     @vimeo_video_id = @video.link[/\d+/]
     @comments = @video.comments
     @comment = Comment.new
+    @fb_url = "http://ulysses.it/#{params[:id]}"
+    @fb_title = "Interview with #{@title}"
+    @fb_video = @video.link
     render "layouts/index.erb"
   end
   
