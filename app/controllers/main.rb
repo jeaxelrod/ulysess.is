be5d85401e42e82c4029ccf515640ef53e6aa1ed
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-04-16 12:38:18 -0700

    Change facebook video url

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -8,7 +8,7 @@ Ulysess::App.controllers  do
     @comment = Comment.new
     @fb_url = "http://ulysses-videos.herokuapp.com/"
     @fb_title = "Interview with #{@title}"
-    @fb_video = "http://vimeo.com/moogaloop.swf?clip_id=16034713&amp;force_embed=1&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=1&amp;color=00adef&amp;fullscreen=1&amp;autoplay=0&amp;loop=0" 
+    @fb_video = "http://vimeo.com/moogaloop.swf?clip_id=#{@vimeo_video_id}&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=1&amp;color=00ADEF&amp;fullscreen=1&amp;autoplay=0&amp;loop=0" 
     @fb_image = "http://i.vimeocdn.com/video/97507588_640.jpg"
     render "layouts/index.erb"
   end
@@ -24,9 +24,10 @@ Ulysess::App.controllers  do
     @vimeo_video_id = @video.link[/\d+/]
     @comments = @video.comments
     @comment = Comment.new
-    @fb_url = "http://ulysses-videos.herokuapp.com/#{params[:id]}"
+    @fb_url = "http://ulysses-videos.herokuapp.com/"
     @fb_title = "Interview with #{@title}"
-    @fb_video = @video.link
+    @fb_video = "http://vimeo.com/moogaloop.swf?clip_id=#{@vimeo_video_id}&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=1&amp;color=00ADEF&amp;fullscreen=1&amp;autoplay=0&amp;loop=0" 
+    @fb_image = "http://i.vimeocdn.com/video/97507588_640.jpg"
     render "layouts/index.erb"
   end
  
