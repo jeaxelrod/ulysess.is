d9a7d11ceeb3032522aa75251c3e5ead06642b9c
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-04-16 12:31:06 -0700

    Fix typos

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -29,9 +29,5 @@ Ulysess::App.controllers  do
     @fb_video = @video.link
     render "layouts/index.erb"
   end
-  
-  <!-- This version of the embed code is no longer supported. Learn more: https://vimeo.com/s/tnm --> <object width="800" height="450"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=16034713&amp;force_embed=1&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=1&amp;color=00adef&amp;fullscreen=1&amp;autoplay=0&amp;loop=0" /><embed src=
-  "http://vimeo.com/moogaloop.swf?clip_id=16034713&amp;force_embed=1&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=1&amp;color=00adef&amp;fullscreen=1&amp;autoplay=0&amp;loop=0" 
-  type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="800" height="450"></embed></object> <p><a href="http://vimeo.com/16034713">Interview with Naval Ravikant of VentureHacks</a> from <a href="http://vimeo.com/pointabout">PointAbout, Inc.</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
-
+ 
 end
