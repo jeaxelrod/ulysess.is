f963bfb9bb7fdff64f568c6eebc498abb3909fee
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-05-16 13:28:27 -0700

    Fix typo in boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
-S# Defines our constants
+# Defines our constants
 RACK_ENV = ENV['RACK_ENV'] ||= 'development'  unless defined?(RACK_ENV)
 PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
 
