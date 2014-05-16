0d97cd76e4a39b737841ebd3238a2ad9038b0109
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-05-16 13:27:22 -0700

    Boot.rb for S3 settings

--- config/boot.rb
+++ config/boot.rb
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['SECRET_ACCESS_KEY'],                        # required
     :region                 => 'eu-west-1'                 # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'public/images'                     # required
+  config.fog_directory  = 'Ulysses'                     # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
