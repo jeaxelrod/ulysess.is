09a198ea4df3c49039016a8157eb6c17c1fe1ed1
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-05-19 13:03:09 -0700

    Make s3 folder public

--- config/boot.rb
+++ config/boot.rb
@@ -16,7 +16,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['SECRET_ACCESS_KEY']                       # required
   }
   config.fog_directory  = 'Ulysses'                     # required
-  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_public     = true                                  # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
 
